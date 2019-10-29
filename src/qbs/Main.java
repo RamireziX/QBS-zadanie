4f91d9d5e6c0fe16daf4f6bd1588f65b5e8e39aa
Alexander Wrzosek <alexander.wrzosek@o2.pl>
2019-10-29 11:18:44 +0100

    znajduje folder

--- src/qbs/Main.java
+++ src/qbs/Main.java
@@ -1,16 +1,24 @@
 package qbs;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
 	    Scanner scan = new Scanner(System.in);
 	    System.out.println("Wpisz sciezke folderu, rozszerzenie, ciag bitow 1 ciag bitow 2");
-	    String folder = scan.nextLine();
+	    String pathToFolder = scan.nextLine();
 	    String extention = scan.nextLine();
 	    String replaceBytes = scan.nextLine();
 	    String toBeReplacedBytes = scan.nextLine();
 
-	    System.out.printf("%s, %s, %s, %s", folder, extention, replaceBytes, toBeReplacedBytes);
+	    File dir = new File(pathToFolder);
+
+	    if(dir.exists())
+	    	System.out.println("folder found");
+	    else
+	    	System.out.println("not found");
+
+	    //System.out.printf("%s, %s, %s, %s", folder, extention, replaceBytes, toBeReplacedBytes);
     }
 }
