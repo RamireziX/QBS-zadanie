02ffb4b65ecd2763a29d7657124d14fc35616238
Alexander Wrzosek <alexander.wrzosek@o2.pl>
2019-10-30 17:55:25 +0100

    Reading bytes from file
    add reading bytes from 1 file to byte array

--- src/qbs/Main.java
+++ src/qbs/Main.java
@@ -1,6 +1,7 @@
 package qbs;
 import java.util.*;
 import java.io.*;
+import java.nio.file.Files;
 
 public class Main {
 
@@ -13,21 +14,36 @@ public class Main {
 	    String replaceBytes = scan.nextLine();
 	    String toBeReplacedBytes = scan.nextLine();
 
+		//find folder (for testing dir is now a file, later make a list of files with the same extention)
 	    File dir = new File(pathToDir);
-
-		//find folder
 	    if(dir.exists())
 	    	System.out.println("folder found");
 	    else
 	    	System.out.println("not found");
 
 	    //find files with given extention
-		ExtentionFilter extFilter = new ExtentionFilter(extention);
+		/*ExtentionFilter extFilter = new ExtentionFilter(extention);
 		String[] filesWithExt;
 		filesWithExt = dir.list(extFilter);
 		for(String name:filesWithExt)
 			System.out.println(name);
+		*/
 
+		//get byte array of a file
+		try {
+			byte[] fileBytes = Files.readAllBytes(dir.toPath());
+
+			/*//print those bytes
+			for(byte b:fileBytes){
+				String s = String.valueOf(b);
+				System.out.print(s + " ");
+			}*/
+		}
+		catch(IOException e){
+			System.out.println("Error reading bytes from file");
+		}
+
+		//debug check input
 	    //System.out.printf("%s, %s, %s, %s", folder, extention, replaceBytes, toBeReplacedBytes);
     }
 }
