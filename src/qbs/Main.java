5531af227d4b1010609765e40d88320e8e90524c
Alexander Wrzosek <alexander.wrzosek@o2.pl>
2019-11-15 13:31:14 +0100

    Wrapping up

--- src/qbs/Main.java
+++ src/qbs/Main.java
@@ -6,10 +6,13 @@ import java.nio.file.Files;
 public class Main {
 
 	/*TODO:
-	   wrap up
+	   add something if user inputs wrong extention (without .) or wrong bytes
 	*/
 	//replace bytes in given files
 	static void replaceBytes(List<File> files, String replaceBytes, String toBeReplacedBytes){
+		System.out.println("Replacing bytes in these files:");
+		int count = 0;
+
 		for(File file:files) {
 			try {
 				//get bytes from 1 file
@@ -22,9 +25,15 @@ public class Main {
 					bytesAsString = bytesAsString.concat(s + " ");
 				}
 
-				//replace bytes
-				bytesAsString = bytesAsString.replaceAll(replaceBytes, toBeReplacedBytes);
-				String[] oneByte = bytesAsString.split(" ");
+				if(bytesAsString.contains(replaceBytes))
+					//replace bytes
+					bytesAsString = bytesAsString.replaceAll(replaceBytes, toBeReplacedBytes);
+				else
+					//does not contain this byte pattern, skip
+					continue;
+
+				//put spaces in between bytes
+					String[] oneByte = bytesAsString.split(" ");
 
 				//parse strings as bytes
 				for (int i = 0; i < oneByte.length; i++) {
@@ -40,10 +49,14 @@ public class Main {
 				os.write(fileBytes);
 				os.close();
 
+				System.out.println(file);
+				count ++;
+
 			} catch (IOException e) {
 				System.out.println(e);
 			}
 		}
+		System.out.println("Replacement successful for " + count + " files");
 	}
 
 	//returns array of files with given extention
@@ -67,29 +80,43 @@ public class Main {
 		return filesWithExt;
 	}
 
+	//checks if folder exists and returs it
+	static File folderExists(String pathToDir, Scanner scan){
+		File dir = new File(pathToDir);
+
+		if(dir.exists()) {
+			System.out.println("Folder found");
+			return dir;
+		}
+		else {//in case folder is not found
+			System.out.println("Folder not found, write the path again:");
+			pathToDir = scan.nextLine();
+			return folderExists(pathToDir, scan);
+		}
+	}
+
     public static void main(String[] args) {
     	//input from user
 	    Scanner scan = new Scanner(System.in);
 
-	    System.out.println("Wpisz sciezke folderu, rozszerzenie, ciag bitow 1 ciag bitow 2");
+	    System.out.println("Write path to folder and press enter:");
 	    String pathToDir = scan.nextLine();
+		System.out.println("Write extention (.your_extention) and press enter:");
 	    String extention = scan.nextLine();
+		System.out.println("Write bytes you want to replace (in decimal, separate each byte by space) and press enter:");
 	    String replaceBytes = scan.nextLine();
+		System.out.println("Write bytes you want to switch previously found bytes (in decimal, separate each byte by space) and press enter:");
 	    String toBeReplacedBytes = scan.nextLine();
-
-	    scan.close();
 
 	    //create filter
 		ExtentionFilter extFilter = new ExtentionFilter(extention);
 
-		//find folder
-	    File dir = new File(pathToDir);
-	    if(dir.exists())
-	    	System.out.println("folder found");
-	    else
-	    	System.out.println("not found");
+		//look for folder
+		File dir = folderExists(pathToDir, scan);
 
 		//replace bytes
 		replaceBytes(findFilesByExt(extFilter, dir), replaceBytes, toBeReplacedBytes);
+
+		scan.close();
     }
 }
